315a12a300993af6d577ae3e653507317ef2ff9d
Halit <xltcamm@gmail.com>
2025-10-28 23:59:00 +0100

    Card Components from Chakra added

---
+++ src/components/Card/index.js
@@ -0,0 +1,13 @@
+import {Box, Image, Button } from "@chakra-ui/react"
+
+import React from 'react';
+
+const Card = () => {
+    return (
+        <div>
+            Card
+        </div>
+    );
+}
+
+export default Card;
--- src/pages/Products/index.js
+++ src/pages/Products/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Card from "../Card/index"
 
 const Products = () => {
     return (
         <div>
-            Products Lists
+            <Card/>
         </div>
     );
 }
