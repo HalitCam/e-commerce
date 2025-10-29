3802e19d06041b6824791a52ed1c1d7f8facc6e8
Halit <xltcamm@gmail.com>
2025-10-29 23:59:27 +0100

    changes

--- src/pages/Products/index.js
+++ src/pages/Products/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import Card from "../Card/index"
+import Card from "../../components/Card"
 
 const Products = () => {
     return (
         <div>
+            <h1 style={display:flex textAlign:center bold textcColor:gray} ">Products Lists</h1>
             <Card/>
         </div>
     );
