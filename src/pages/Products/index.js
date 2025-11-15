15b6c833c2e11f7529c571dce05446ee1f01a54b
Halit <xltcamm@gmail.com>
2025-11-16 00:07:36 +0100

    useInfiniteQuery

--- src/pages/Products/index.js
+++ src/pages/Products/index.js
@@ -1,25 +1,27 @@
 import React from 'react';
 import Card from "../../components/Card"
-import {Grid, GridItem} from "@chakra-ui/react"
-import {useQuery } from 'react-query'
-import {fetchProductList} from "../../api.js"
+import { Grid, GridItem } from "@chakra-ui/react"
+import { useInfiniteQuery } from 'react-query'
+import { fetchProductList } from "../../api.js"
 
 
 
 const Products = () => {
-    const { isLoading, error, data } = useInfiniteQuery('repoData', fetchProductList)
+  const { isLoading, error, data } = useInfiniteQuery('repoData', fetchProductList, {
+    getNextPageParam: (lastGroup, pages) => lastGroup.nextCursor
+  })
 
   if (isLoading) return 'Loading...'
 
   if (error) return 'An error has occurred: ' + error.message
 
-    return (
-            <Grid templateColumns='repeat(4, 1fr)' gap={3}>
-            {
-                data.map((item,key) => <Card key={key} item={item}/>)
-            }
-            </Grid>
-    );
+  return (
+    <Grid templateColumns='repeat(4, 1fr)' gap={3}>
+      {
+        data.map((item, key) => <Card key={key} item={item} />)
+      }
+    </Grid>
+  );
 }
 
 export default Products;
