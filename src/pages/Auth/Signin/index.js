13b0eda86032b5cd53013b323d4c99dff106688e
Halit <xltcamm@gmail.com>
2025-12-06 23:37:01 +0100

    for signin page is beimg constructed

--- src/pages/Auth/Signin/index.js
+++ src/pages/Auth/Signin/index.js
@@ -1,11 +1,34 @@
 import React from 'react';
+import { Text, Button, Flex, Box, Input, FormLabel } from '@chakra-ui/react';
+import { Formik } from 'formik';
+
 
 const Signin = () => {
     return (
         <div>
-            Signin
+            <Formik>
+                <Flex justifyContent="center" textAlign="center" > 
+                    <Box >
+                        <Text fontSize="55px"> Sign In</Text>
+                    </Box>
+                    <Box>
+                        <form>
+                            <FormLabel> Email:
+                            <Input placeholder='Enter your email'/>
+                            </FormLabel>
+                            <FormLabel> Password:
+                            <Input placeholder='Enter your password'/>
+                            </FormLabel>
+                            <Button mt="5px">Sign in</Button>
+                        </form>
+                    </Box>
+
+                </Flex>
+            </Formik>
+
         </div>
     );
 }
 
 export default Signin;
+
