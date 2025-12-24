3ca33af49ea4dc4b58261993c471eba2c132c9f2
Halit <xltcamm@gmail.com>
2025-12-24 23:44:16 +0100

    At admin:product page edit and delete a tags were regulated

--- src/api.js
+++ src/api.js
@@ -94,6 +94,13 @@ export const fetchOrders = async() => {
 	return data;
 }
 
+export const deleteProduct = async (product_id) =>{
+	const {data} = await axios.delete(
+		`${process.env.REACT_APP_BASE_ENDPOINT}/product/${product_id}`
+	);
+	return data;
+}
+
 
 
 
--- src/pages/Admin/Products/index.js
+++ src/pages/Admin/Products/index.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import { useQuery } from "react-query";
-import { fetchProductList } from '../../../api';
+import React, { useMemo } from 'react';
+import { useQuery, useMutation, useQueryClient } from "react-query"; // Server dan veri cekme Query ile yapilirken; veri düzenleme, ekleme, silme gibi
+// islemeler useMutation ile yapilir !!
+import { fetchProductList, deleteProduct } from '../../../api';
 
 import { Link } from "react-router-dom";
 import { Text } from "@chakra-ui/react";
@@ -8,7 +9,66 @@ import { Popconfirm, Table } from "antd"; // This UI Ant Design is besser at Tab
 import moment from "moment";
 
 const Products = () => {
-    const { isLoading, isError, data, error } = useQuery("admin:products", fetchProductList);
+    const { isLoading, isError, data, error } = useQuery(
+        "admin:products", fetchProductList
+    );
+
+    const queryClient = useQueryClient();
+
+    const deleteMutation = useMutation(deleteProduct, {
+        onSuccess: () => {
+            queryClient.invalidateQueries("admin:products")
+        }
+    })
+
+    const columns = useMemo(() => {
+        return [
+            {
+                title: "Title",
+                dataIndex: "title",
+                key: "title"
+            },
+            {
+                title: 'Price',
+                dataIndex: 'price',
+                key: 'price'
+            },
+            {
+                title: 'Created At',
+                dataIndex: 'createdAt',
+                key: 'createdAt'
+            },
+            {
+                title: 'Action',
+                key: 'action',
+                render: (text, record) => (
+                    <>
+                        <Link to={`/admin/products/${record.key}`}>Edit</Link>
+                        <Popconfirm
+                            title="Are you sure ?"
+                            onConfirm={() => {
+                                deleteMutation.mutate(record.key, {
+                                    onSuccess: () => {
+                                        console.log("success")
+                                    }
+                                })
+                            }}
+                            onCancel={() => {
+                                console.log("cancelled")
+                            }}
+                            placement='left'
+                            okText="Yes"
+                            cancelText="No"
+                        >
+                            <a href='#' style={{ marginLeft: 10 }}>Delete</a>
+                        </Popconfirm>
+                    </>
+                )
+            }
+        ]
+    }, [])
+
+
 
     if (isLoading) {
         return <div>Loading...</div>
@@ -17,46 +77,7 @@ const Products = () => {
         return <div> Error: {error.message} </div>
     }
 
-    const columns = [
-        {
-            title: "Title",
-            dataIndex: "title",
-            key: "title"
-        },
-        {
-            title: 'Price',
-            dataIndex: 'price',
-            key: 'price'
-        },
-        {
-            title: 'Created At',
-            dataIndex: 'createdAt',
-            key: 'createdAt'
-        },
-        {
-            title: 'Action',
-            key: 'action',
-            render: (text, record) => (
-                <>
-                    <Link to={`/admin/products/${record.key}`}>Edit</Link>
-                    <Popconfirm
-                        title="Are you sure ?"
-                        onConfirm={() => {
-                            alert("deleted")
-                        }}
-                        onCancel={() => {
-                            console.log("canceled")
-                        }}
-                        placement='left'
-                        okText="Yes"
-                        cancelText="No"
-                    >
-                        <a href='#' style={{marginLeft: 10}}>Delete</a>
-                    </Popconfirm>
-                </>
-            )
-        }
-    ]
+
     // Normally man kan give to dataSource={data} data... this will be very solution. But in this process you muss give at the bottom 
     //in return block "rowKey= "_id" ""
     const dataSource = data.map((item) => (
