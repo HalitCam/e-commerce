2c6befe300c626d7845cdba6d689906793b7d902
Halit <xltcamm@gmail.com>
2025-12-27 09:22:33 +0100

    Edit page is being regulated

--- src/pages/Admin/ProductDetail/index.js
+++ src/pages/Admin/ProductDetail/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import {useParams} from 'react-router-dom';
+import {Text} from "@chakra-ui/react";
 
 import { fetchProduct } from '../../../api';
 import {useQuery} from "react-query";
+import {Formik} from "formik";
 
 const ProductDetail = () => {
     const {product_id} = useParams();
 
-    const {isError, isLoading,  data, error } = useQuery(["admin:product", product_id], fetchProduct(product_id));
+    const {isError, isLoading,  data, error } = useQuery(["admin:product", product_id], ()=>fetchProduct(product_id));
+
+    if(isLoading){
+        return <div>Loading...</div>
+    }
+    if(isError){
+        return <div>Error {error.message}</div>
+    }
  
     return (
         <div>
-            ProductDetails
+           <Text fontSize="2xl"> Edit </Text>
+           <Formik 
+           initialValues={{
+            
+           }}>
+
+           </Formik>
         </div>
     );
 }
