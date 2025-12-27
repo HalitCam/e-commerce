cd607555c6c95cd3a8d30dae86928b777428a3e3
Halit <xltcamm@gmail.com>
2025-12-27 09:31:42 +0100

    edit

--- src/pages/Admin/ProductDetail/index.js
+++ src/pages/Admin/ProductDetail/index.js
@@ -1,32 +1,41 @@
 import React from 'react';
-import {useParams} from 'react-router-dom';
-import {Text} from "@chakra-ui/react";
+import { useParams } from 'react-router-dom';
+import { Text } from "@chakra-ui/react";
 
 import { fetchProduct } from '../../../api';
-import {useQuery} from "react-query";
-import {Formik} from "formik";
+import { useQuery } from "react-query";
+import { Formik } from "formik";
 
 const ProductDetail = () => {
-    const {product_id} = useParams();
+    const { product_id } = useParams();
 
-    const {isError, isLoading,  data, error } = useQuery(["admin:product", product_id], ()=>fetchProduct(product_id));
+    const { isError, isLoading, data, error } = useQuery(["admin:product", product_id], () => fetchProduct(product_id));
 
-    if(isLoading){
+    if (isLoading) {
         return <div>Loading...</div>
     }
-    if(isError){
+    if (isError) {
         return <div>Error {error.message}</div>
     }
- 
+    const handleSubmit = () =>{
+        console.log("submitted")
+    }
+
     return (
         <div>
-           <Text fontSize="2xl"> Edit </Text>
-           <Formik 
-           initialValues={{
-            
-           }}>
+            <Text fontSize="2xl"> Edit </Text>
+            <Formik
+                initialValues={{
+                    title: data.title,
+                    description: data.description,
+                    price: data.price,
+                    photos: data.photos,
+                }}
+                //    validationSchema={}
+                onSubmit={handleSubmit}
+            >
 
-           </Formik>
+            </Formik>
         </div>
     );
 }
