8d1cb43b464df73ccfcd9315e8b2ee6547cc89e9
Halit <xltcamm@gmail.com>
2025-12-31 16:29:17 +0100

    Product update page completed

--- src/api.js
+++ src/api.js
@@ -87,20 +87,25 @@ export const postOrder = async (input) => {
 	return data;
 }
 
-export const fetchOrders = async() => {
-	const {data}= await axios.get(
+export const fetchOrders = async () => {
+	const { data } = await axios.get(
 		`${process.env.REACT_APP_BASE_ENDPOINT}/order`
 	)
 	return data;
 }
 
-export const deleteProduct = async (product_id) =>{
-	const {data} = await axios.delete(
+export const deleteProduct = async (product_id) => {
+	const { data } = await axios.delete(
 		`${process.env.REACT_APP_BASE_ENDPOINT}/product/${product_id}`
 	);
 	return data;
 }
 
+export const updateProduct = async (product_id, input) => {
+	const { data } = await axios.put(`${process.env.REACT_APP_BASE_ENDPOINT}/product/${product_id}`,
+		input)
+	return data;
+}
 
 
 
--- src/pages/Admin/ProductDetail/index.js
+++ src/pages/Admin/ProductDetail/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import { Text } from "@chakra-ui/react";
+import { Text, FormControl, FormLabel, Input, Box, Button, Textarea } from "@chakra-ui/react";
 
-import { fetchProduct } from '../../../api';
+import { fetchProduct, updateProduct } from '../../../api';
 import { useQuery } from "react-query";
-import { Formik } from "formik";
+import { FieldArray, Formik } from "formik";
+import validationSchema from "./validation";
+import { message } from "antd";
 
 const ProductDetail = () => {
     const { product_id } = useParams();
@@ -17,25 +19,123 @@ const ProductDetail = () => {
     if (isError) {
         return <div>Error {error.message}</div>
     }
-    const handleSubmit = () =>{
-        console.log("submitted")
+    const makeSubmit = async (values, bag) => {
+        console.log("submitted");
+        message.loading({content : "Loading..." , key: "product_update"});
+       
+        try{
+            await updateProduct(product_id, values);
+            message.success({
+                content : "The product successfully updated",
+                key: "product_update",
+                duration: 2 ,
+            });
+        }catch(error){
+            message.error("The product does not updated !")
+        }
+
     }
+    
 
     return (
         <div>
             <Text fontSize="2xl"> Edit </Text>
+
             <Formik
-                initialValues={{
-                    title: data.title,
-                    description: data.description,
-                    price: data.price,
-                    photos: data.photos,
-                }}
-                //    validationSchema={}
-                onSubmit={handleSubmit}
+                initialValues={
+                    {
+                        title: data.title,
+                        description: data.description,
+                        price: data.price,
+                        photos: data.photos
+                    }
+                }
+                validationSchema={validationSchema}
+                onSubmit={makeSubmit}
+
             >
+                {
+                    ({ handleSubmit, handleChange, handleBlur, errors, touched, values, isSubmitting }) => (
+                        <Box my="5" textAlign="left">
+                            <form onSubmit={handleSubmit} >
+                                <FormControl>
+                                    <FormLabel>Title</FormLabel>
+                                    <Input
+                                        name="title"
+                                        value={values.title}
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        disabled={isSubmitting}
+                                        isInvalid={touched.title && errors.title}
+                                    />
+                                    {errors.title && touched.title && <Text color='red.400'>{errors.title}</Text>}
+
+                                </FormControl>
+                                <FormControl mt="4">
+                                    <FormLabel>Description</FormLabel>
+                                    <Textarea
+                                        name="description"
+                                        value={values.description}
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        disabled={isSubmitting}
+                                        isInvalid={touched.description && errors.description}
+                                    />
+                                    {errors.description && touched.description && <Text color='red.400'>{errors.description}</Text>}
+
+                                </FormControl>
+                                <FormControl mt="4">
+                                    <FormLabel>Price</FormLabel>
+                                    <Input
+                                        type="number"
+                                        name="price"
+                                        value={values.price}
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        disabled={isSubmitting}
+                                        isInvalid={touched.price && errors.price}
+                                    />
+                                    {errors.price && touched.price && <Text color='red.400'>{errors.price}</Text>}
+                                </FormControl>
+                                <FormControl mt="4">
+                                    <FormLabel>Photos</FormLabel>
+                                    <FieldArray
+                                        name="photos"
+                                        render={(arrayHelpers) => (
+                                            <div>
+                                                {
+                                                    values.photos && values.photos.map((photo, index) => (
+                                                        <div key={index}>
+                                                            <Input
+                                                                width="3xl"
+                                                                name={`photos.${index}`}
+                                                                value={photo}
+                                                                disabled={isSubmitting}
+                                                                onChange={handleChange}
+
+                                                            />
+                                                            <Button ml="4" type='button' color='red' onClick={() => arrayHelpers.remove(index)}>
+                                                                Remove
+                                                            </Button>
+                                                        </div>
+                                                    ))
+                                                }
+                                                <Button mt="5" type='button' onClick={() => arrayHelpers.push("")}>Add a Photo</Button>
+                                            </div>
+                                        )}
+                                    />
+                                </FormControl>
+
+
+                                <Button isLoading={isSubmitting} mt="10px" width="full" type='submit'>Update</Button>
+                            </form>
+                        </Box>
+                    )
+                }
 
             </Formik>
+
+
         </div>
     );
 }
